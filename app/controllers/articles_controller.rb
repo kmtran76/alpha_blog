123b120c246bcb5a531c4f4e2ce34f4d1ac125a5
K Tran <trank76@gmail.com>
2020-12-02 14:56:45 -0500

    Add articles listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,11 @@ class ArticlesController < ApplicationController
     # @ converts the regular variable into an instance variable so
     # it can be used in the view(UI)
     
-    byebug
+    # byebug
     @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
   end
 end
