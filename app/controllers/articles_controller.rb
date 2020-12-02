7f9602ff9d0efbb474f4189b343fee0065b58e6f
K Tran <trank76@gmail.com>
2020-12-02 16:34:20 -0500

    Add ability to create new articles from front-end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,4 +11,25 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+  end
+
+  def create
+    # render plain: params[:article]
+    
+    # need to whitelist the title and description from article key 
+    # otherwise it wont work
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    
+    #render plain: @article.inspect
+    @article.save 
+
+    # (rails routes --expanded) tell you the show route is using prefix article
+    # to use the prefix you need to append the _path
+    redirect_to article_path(@article)
+
+    # shortern path has the same affect as redirect_to article_path(@article)
+    # redirect_to @article
+  end
 end
