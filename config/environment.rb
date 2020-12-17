0a888c2f4009c7e35a0b2d08b912244eefc20aa8
K Tran <trank76@gmail.com>
2020-12-17 09:05:07 -0500

    Style validation and flash messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# To remove the field_with_errors functionality that added by rails 
+# when there is a field that hit the validation error 
+# (But we want to disable this feature from rails  by insert the following code below) 
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
