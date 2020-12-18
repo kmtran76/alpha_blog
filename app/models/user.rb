31be75ace234dbb7ebefe670b56eb30f75d0679d
K Tran <trank76@gmail.com>
2020-12-18 14:53:38 -0500

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-
+  # can read more at https://guides.rubyonrails.org/active_record_validations.html
+  validates :username, presence: true, 
+                       uniqueness: { case_sensitive: false }, 
+                       length: {minimum: 3, maximum: 25}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+                    uniqueness: { case_sensitive: false }, 
+                    length: {maximum: 105},
+                    format: {with: VALID_EMAIL_REGEX}
 end
