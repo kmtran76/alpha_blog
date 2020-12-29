579a9452404dc5871cc64ccf177c3a64a83b135c
K Tran <trank76@gmail.com>
2020-12-29 11:19:08 -0500

    Add auth helper methods and modify nav partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show    
     # @articles = @user.articles
     # use will_paginate gem below https://github.com/mislav/will_paginate
     @articles =  @user.articles.paginate(page: params[:page], per_page:2)
@@ -17,11 +18,10 @@ class UsersController < ApplicationController
   end
 
   def edit 
-    @user = User.find(params[:id])
+    
   end 
 
-  def update
-    @user = User.find(params[:id])
+  def update    
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to @user      # Note: @user is short for user_path
@@ -34,6 +34,7 @@ class UsersController < ApplicationController
     #byebug
     @user = User.new(user_params)
     if @user.save 
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
@@ -45,5 +46,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
